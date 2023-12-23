f5d812257e4eb12b42fc4d760104e6eb28d366f5
venkat10raman <10venkataramana@gmail.com>
2023-12-23 16:02:57 +0530

    added open-api description

--- src/main/java/com/venkat/airlinewebapp/WebConfig.java
+++ src/main/java/com/venkat/airlinewebapp/WebConfig.java
@@ -1,10 +1,15 @@
 package com.venkat.airlinewebapp;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.venkat.airlinewebapp.aop.LoggingInterceptor;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 
 @Configuration
@@ -13,6 +18,16 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new LoggingInterceptor());
+	}
+	
+	@Bean
+	public OpenAPI openApiConfig() {
+		return new OpenAPI().info(
+				new Info().title("Airline RESTFul Web Application")
+				.version("1.0.0")
+				.description("R for RESTFul Web Services")
+				.termsOfService("http://venkat.com/terms")
+				.license(new License().name("Venkat License").url("http://venkat.com")));
 	}
 
 }
